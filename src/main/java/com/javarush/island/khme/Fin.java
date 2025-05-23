1d1d263b2e65c4a7d9e9f8b78bba97a54b9e5589
akhmelev <akhmelev@gmail.com>
2025-05-23 19:58:53 +0300

    JavaFX version

--- src/main/java/com/javarush/island/khme/Fin.java
+++ src/main/java/com/javarush/island/khme/Fin.java
@@ -2,6 +2,6 @@ package com.javarush.island.khme;
 
 public class Fin {
     public static void main(String[] args) {
-        System.out.println("Fin! 100500 Animals!!!");
+        System.out.println("JavaFX Fin! 100500 Animals!!!");
     }
 }
